fa7a762a96545bd83e75ca0203d7bfec0a140d06
lisovenko02 <vanyalisovenkoo@gmail.com>
2025-03-03 16:52:44 +0200

    update cors https

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 	app.setGlobalPrefix('api')
 	app.use(cookieParser())
 	app.enableCors({
-		origin: ['http://localhost:3000'],
+		origin: ['https://trello-front-ebon.vercel.app/'],
 		credentials: true,
 		exposedHeaders: 'set-cookie',
 	})
